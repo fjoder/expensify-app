6090752434b8d63bf257fe223da26405f00764ae
Franklin Oder <foder@etech-group.com>
2017-11-02 17:06:03 -0400

    quick edit

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import 'normalize.css/normalize.css';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
-import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 0 }));
 store.dispatch(addExpense({ description: 'Gas bill', amount: 1000, createdAt: 1000 }));
