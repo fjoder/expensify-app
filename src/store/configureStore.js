154fc49ef47920383be770fbd3e1ceecfc11338f
Franklin Oder <fjoder@gmail.com>
2017-11-03 00:47:22 -0400

    completed redux integration, inital test case setup

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,12 +4,15 @@ import filtersReducer from '../reducers/filters';
 
 // Store creation
 export default () => {
+  /* eslint-disable no-underscore-dangle */
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducer
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
+  /* eslint-enable */
 
   return store;
 };
